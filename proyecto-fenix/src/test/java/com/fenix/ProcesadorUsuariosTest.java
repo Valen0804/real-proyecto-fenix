1fa0a2a5ad714e31a903e5037c0183beb7fc5e91
Valentin Fuentes <valenfgxd@gmail.com>
2026-04-27 08:00:43 +0200

    He renombrado correctamente el archivo de test

---
+++ proyecto-fenix/src/test/java/com/fenix/ProcesadorUsuariosTest.java
@@ -0,0 +1,31 @@
+package com.fenix;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Pruebas unitarias para ProcesadorUsuarios.
+ */
+class ProcesadorUsuariosTest {
+
+    @Test
+    void testProcesarListaComportamientoCorrecto() {
+        ProcesadorUsuarios procesador = new ProcesadorUsuarios();
+
+        List<String> lista = List.of(
+                "Ana:1",
+                "Luis:2",
+                "Eva:1",
+                "Juan:99"
+        );
+
+        String resultado = procesador.procesarLista(lista);
+
+        assertEquals(
+                "Admins: Ana,Eva | Invitados: Luis",
+                resultado
+        );
+    }
+}
